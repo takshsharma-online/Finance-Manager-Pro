864f94fdaae9bd06b1a19e494db01d5c6e971d85
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-03 22:51:27 +0100

    Add for transactions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import database.DatabaseManager;
+import database.TransactionDAO;
+import model.Transaction;
 import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +9,18 @@ public class Main {
             System.out.println("Connected!");
         }
         DatabaseManager.createTransactionsTable();
+        //Test for transactions.
+        Transaction transaction =
+                new Transaction(
+                        0,
+                        "2025-06-02",
+                        "Expense",
+                        "Food",
+                        15.50,
+                        "Lunch"
+                );
+        TransactionDAO dao = new TransactionDAO();
+        dao.addTransaction(transaction);
+        System.out.println(dao.getAllTransactions());
     }
 }
