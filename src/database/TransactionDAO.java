13628eb77ce53f3181b51a461cf567f3fc8b8ac7
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-05 18:04:46 +0100

    Added method for deleting a transaction.

--- src/database/TransactionDAO.java
+++ src/database/TransactionDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionDAO {
+    //Following is the method for adding a transaction.
     public void addTransaction(Transaction transaction) {
         String sql = """
                 INSERT INTO transactions
@@ -29,6 +30,7 @@ public class TransactionDAO {
             e.printStackTrace();
         }
     }
+    //Following is the method for updating a transaction.
     public void updateTransaction(Transaction transaction){
         String sql = """
                 UPDATE transactions
@@ -59,7 +61,28 @@ public class TransactionDAO {
             e.printStackTrace();
         }
     }
-
+    //Following is the method for deleting a transaction.
+    public void deleteTransaction(int id){
+        String sql = """
+                DELETE from transactions
+                WHERE id = ?
+                """;
+        try (
+                Connection conn = DatabaseManager.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            pstmt.setInt(1, id);
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Transaction deleted successfully!");
+            } else {
+                System.out.println("Transaction not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //Following is a method to get all the transactions using arrays.
     public List<Transaction> getAllTransactions() {
         List<Transaction> transactions = new ArrayList<>();
         String sql = "SELECT * FROM transactions";
