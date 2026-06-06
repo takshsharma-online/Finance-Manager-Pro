b6719ac870579ed5fbc19fab6da59c0211b295f6
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-06 11:57:36 +0100

    Added categories

---
+++ src/model/Category.java
@@ -0,0 +1,26 @@
+package model;
+
+public enum Category {
+    //Expense Categories
+    FOOD,
+    TRANSPORT,
+    RENT,
+    UTILITIES,
+    SHOPPING,
+    ENTERTAINMENT,
+    HEALTHCARE,
+    EDUCATION,
+    //Income Categories
+    SALARY,
+    FREELANCE,
+    BUSINESS,
+    INVESTMENTS,
+    BONUS,
+    GIFTS,
+    REFUND,
+    CRYPTO,
+    //Others
+    SAVINGS,
+    OTHER
+}
+
