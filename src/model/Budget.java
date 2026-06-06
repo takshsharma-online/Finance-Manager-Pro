4584a614eb109d5f19852b48cae35288dd76a6d5
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-06 20:52:30 +0100

    Added attributes.
    Added attributes for id, category, monthly limit, currency.

--- src/model/Budget.java
+++ src/model/Budget.java
@@ -1,4 +1,8 @@
 package model;
 
-public class Budget {
+public class Budget {//The main goal of this class is to allow user to create a budget.
+    private int id;//Attribute for id.
+    private String category;//Attribute for category.
+    private double monthlyLimit;//Attribute for monthly limit.
+    private Currency currency;//Attribute for currency.
 }
