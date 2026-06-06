a6d6701b1bbde333c1cbe85c9484a628d2451808
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-06 21:14:39 +0100

    Added a test to check new budget classes.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
+import database.BudgetDAO;
 import database.DatabaseManager;
 import database.TransactionDAO;
+import model.Budget;
+import model.Currency;
 import model.Transaction;
 import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DatabaseManager.connect();
-        if(conn != null) {
-            System.out.println("Connected!");
-        }
-        DatabaseManager.createTransactionsTable();
-
-        //Test for deleting transaction.
-        TransactionDAO dao = new TransactionDAO();
-
-        dao.deleteTransaction(1);
+        //Following is a test for new budget classes.
+        BudgetDAO dao= new BudgetDAO();
+        Budget foodBudget =
+                new Budget(
+                        0,
+                        "Food",
+                        300,
+                        Currency.EUR
+                );
+        dao.addBudget(foodBudget);
+        System.out.println(dao.getAllBudgets());
 
 
     }
