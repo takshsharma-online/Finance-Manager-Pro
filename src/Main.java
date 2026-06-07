905864a8ce39257ca39c0e147b1a4c4b7e6ddde8
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-07 21:06:03 +0100

    Implemented test for deleteBudget() method.

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,18 @@ import model.Transaction;
 import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
-        //Following is a test for new budget classes.
-        BudgetDAO dao= new BudgetDAO();
-        Budget foodBudget =
-                new Budget(
-                        1,
-                        "FOOD",
-                        400,
-                        Currency.GBP
-                );
-        dao.updateBudget(foodBudget);
+    //Following is a test for new budget classes.
+        BudgetDAO dao = new BudgetDAO();
 
+        System.out.println("Before:");
 
-    }
+        System.out.println(dao.getAllBudgets());
+
+        dao.deleteBudget(1);
+
+        System.out.println("After:");
+
+        System.out.println(dao.getAllBudgets());
+
+}
 }
