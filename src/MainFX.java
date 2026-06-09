d528a96f6fae8e9c62edc0a0c53812a0c318c88a
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-09 22:22:03 +0100

    Added dashboard view to mainfx.java

--- src/MainFX.java
+++ src/MainFX.java
@@ -3,17 +3,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import ui.DashboardView;
 
 public class MainFX extends Application {
     @Override
-    public  void start(Stage stage){
-        Label label = new Label("Finance Manager Pro");
-        Scene scene = new Scene( new StackPane(label), 600, 400);
+
+    public void start(Stage stage) {
+        DashboardView dashboard = new DashboardView();
+        Scene scene =
+                new Scene(dashboard, 800,600);
         stage.setTitle("Finance Manager Pro");
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String [] args){
+    public static void main(String[] args) {
         launch();
     }
+
 }
