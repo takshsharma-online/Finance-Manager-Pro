c062e3c9722c934af46b61322ca4ecd2bca80b16
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-18 23:28:46 +0100

    Checked by running the transaction table.

--- src/MainFX.java
+++ src/MainFX.java
@@ -1,18 +1,19 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import ui.DashboardView;
 import ui.TransactionView;
 
 public class MainFX extends Application {
     @Override
-
     public void start(Stage stage) {
-        TransactionView dashboard = new TransactionView();
+        TransactionView transactionView =
+                new TransactionView();
         Scene scene =
-                new Scene(dashboard, 800,600);
+                new Scene(
+                        transactionView,
+                        800,
+                        600
+                );
         stage.setTitle("Finance Manager Pro");
         stage.setScene(scene);
         stage.show();
@@ -20,5 +21,4 @@ public class MainFX extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
