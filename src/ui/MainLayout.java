92e9ad5810b85a69cc4bbcdc3cde0c18d21a5c1a
Taksh Sharma <tsharma2005@protonmail.com>
2026-06-23 20:52:11 +0100

    Added structure for the navigation.

--- src/ui/MainLayout.java
+++ src/ui/MainLayout.java
@@ -1,4 +1,52 @@
 package ui;
-
-public class MainLayout {
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+public class MainLayout extends BorderPane{//This class controls navigation between screens.
+    public MainLayout() {
+        // Navigation buttons.
+        Button dashboardButton =
+                new Button("Dashboard");
+        Button transactionButton =
+                new Button("Transactions");
+        Button budgetButton =
+                new Button("Budgets");
+        // Navigation bar.
+        HBox navigationBar =
+                new HBox(10);
+        navigationBar.setPadding(
+                new Insets(10)
+        );
+        navigationBar.getChildren().addAll(
+                dashboardButton,
+                transactionButton,
+                budgetButton
+        );
+        // Places navigation bar at the top
+        setTop(navigationBar);
+        // Default screen.
+        setCenter(
+                new DashboardView()
+        );
+        // Opens dashboard screen.
+        dashboardButton.setOnAction(event -> {
+            setCenter(
+                    new DashboardView()
+            );
+        });
+        // Opens transaction screen.
+        transactionButton.setOnAction(event -> {
+            setCenter(
+                    new TransactionView()
+            );
+        });
+        // Opens budget screen.
+        budgetButton.setOnAction(event -> {
+            setCenter(
+                    new BudgetView());
+        });
+    }
 }
+
+
